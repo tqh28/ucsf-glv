cc0cb145a302284971dd18bfe6c80043c77f75e7
Huy Quang <tranquanghuy288@gmail.com>
2018-07-13 15:42:11 +0700

    remove /webapi in URL

--- src/main/java/org/ucsf/glv/webapp/config/guice/Config.java
+++ src/main/java/org/ucsf/glv/webapp/config/guice/Config.java
@@ -7,7 +7,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-@ApplicationPath("/webapi")
+@ApplicationPath("")
 public class Config extends ResourceConfig {
 
     public Config() {
